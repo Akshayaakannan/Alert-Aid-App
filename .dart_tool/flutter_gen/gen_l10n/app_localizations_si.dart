d58db45079c338832f680dba14b3fdcc45f3af70
akshaya <akshaya.s.kannan@gmail.com>
2025-09-12 10:53:28 +0530

    add newsfeed

---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_si.dart
@@ -0,0 +1,29 @@
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Sinhala Sinhalese (`si`).
+class AppLocalizationsSi extends AppLocalizations {
+  AppLocalizationsSi([String locale = 'si']) : super(locale);
+
+  @override
+  String get welcome => 'ආයුබෝවන්!';
+
+  @override
+  String get pleaseSignInOrCreate => 'කරුණාකර ඔබගේ ගිණුමට පිවිසෙන්න හෝ නව ගිණුමක් තනන්න';
+
+  @override
+  String get signInWithGoogle => 'Google ඔස්සේ පිවිසෙන්න';
+
+  @override
+  String get signInWithFacebook => 'Facebook ඔස්සේ පිවිසෙන්න';
+
+  @override
+  String get doNotHaveAccount => 'ඔබට ගිණුමක් නැද්ද?';
+
+  @override
+  String get registerNow => 'දැන් ලියාපදිංචි වන්න';
+
+  @override
+  String get signIn => 'පිවිසෙන්න';
+}
